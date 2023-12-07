89fe6a4a5c49266e4b0c8d058a7810e40ca3dac9
minkl <minkl@DESKTOP-PSN3QET>
2023-12-07 22:34:34 +0900

    second commit2

---
+++ 52market/src/main/java/kr/qna/action/ListAction.java
@@ -0,0 +1,16 @@
+package kr.qna.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+
+public class ListAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return "/WEB-INF/views/qna/list.jsp";
+	}
+
+}
